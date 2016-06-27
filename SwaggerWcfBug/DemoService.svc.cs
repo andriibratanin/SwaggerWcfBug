33a09386be8242274a78b3d7c7b6ed41c7d2e7fa
Andrii Bratanin <bra@ciklum.com>
2016-06-27 17:45:58 +0300

    Initial commit

---
+++ SwaggerWcfBug/DemoService.svc.cs
@@ -0,0 +1,61 @@
+﻿using SwaggerWcf.Attributes;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace SwaggerWcfBug
+{
+    //
+    // Traces will be included into "Traces.svclog" file near the project file
+    // Use an url like the following to see Swagger's ui (port number will probably change) http://localhost:58590/api-docs/
+    //
+
+    [ServiceContract]
+    public interface IServiceContract
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/ok")]
+        void SwaggerOk(List<string> ok);
+
+        //comment the following "SwaggerWcfHidden" attribute to reproduce the bug
+        [SwaggerWcfHidden]
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/fail")]
+        void SwaggerFail(List<string> notok);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/arrayOk")]
+        void SwaggerArrayOk(string[] parameter);
+
+        // will be available on an url like the following (port number will probably change) http://localhost:58590/DemoService.svc/json/ping
+        [OperationContract]
+        [WebGet(UriTemplate = "/ping")]
+        string Ping();
+    }
+
+    [SwaggerWcf("/")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class DemoService : IServiceContract
+    {
+        public void SwaggerOk(List<string> ok)
+        {
+            var breakpointPlaceholder = ok;
+        }
+
+        public void SwaggerFail(List<string> notok)
+        {
+            var breakpointPlaceholder = notok;
+        }
+
+        public void SwaggerArrayOk(string[] parameter)
+        {
+            var breakpointPlaceholder = parameter;
+        }
+
+        public string Ping()
+        {
+            return "Pong";
+        }
+    }
+}
---
+++ SwaggerWcfBug/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using SwaggerWcf;
+using System;
+using System.ServiceModel.Activation;
+using System.Web;
+using System.Web.Routing;
+
+namespace SwaggerWcfBug
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.Add(new ServiceRoute("api-docs", new WebServiceHostFactory(), typeof(SwaggerWcfEndpoint)));
+        }
+    }
+}
